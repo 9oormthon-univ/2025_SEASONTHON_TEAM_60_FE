3d89def79e5db8641d0758f23aac1c07c0938645
memymini <minhi0614@gmail.com>
2025-09-16 13:17:04 +0900

    chore:upload response type 추가

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,10 +1,17 @@
 import { api } from "@/lib/fetcher";
+import { Status } from "@/lib/types";
+
+export type UploadResponse = {
+  fileName: string;
+  fileId: string;
+  status: Status;
+};
 
 export const postUpload = (file: File) => {
   const formData = new FormData();
   formData.append("file", file);
 
-  return api.post<void, void>(
+  return api.post<UploadResponse, void>(
     "/certificates/income/upload",
     undefined,
     formData,
