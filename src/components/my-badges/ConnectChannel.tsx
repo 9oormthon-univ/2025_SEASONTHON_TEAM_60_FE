a7970be9c097eaa9d757f61ed2aa5561257c65ad
memymini <minhi0614@gmail.com>
2025-09-19 16:01:38 +0900

    fix:구글 연동 코드 수정

--- src/components/my-badges/ConnectChannel.tsx
+++ src/components/my-badges/ConnectChannel.tsx
@@ -2,13 +2,15 @@ import Button from "@/components/common/Button";
 import Section from "@/components/common/Section";
 
 export default function ConnectChannel() {
-  const GOOGLE_OAUTH_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/google`;
+  const handleConnect = () => {
+    window.location.href = "/oauth2/authorization/google";
+  };
   return (
     <Section className="flex-col">
       <p className="headline-small text-text-primary">
         YouTube 채널에 연결하고 고유 태그 발급받기
       </p>
-      <Button size="md" href={GOOGLE_OAUTH_URL}>
+      <Button size="md" onClick={handleConnect}>
         채널 연동 및 태그 생성하기
       </Button>
     </Section>
